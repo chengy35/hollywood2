9bb48d5cf783c656646d89c46c27c14c67bb040d
chengy35 <chengy35@mail2.sysu.edu.cn>
2017-06-03 22:39:20 +0800

    s

--- getconfig.m
+++ getconfig.m
@@ -1,15 +1,15 @@
 function [video_data_dir,video_dir,fullvideoname, videoname,vocabDir,featDir_FV,featDir_LLC,descriptor_path,actionName] = getconfig()
-    vocabDir = '~/remote/Hollywood2Data/Vocab'; % Path where dictionary/GMM will be saved.
-    featDir_LLC = '~/remote/Hollywood2Data/llc/feats'; % Path where features will be saved
-    featDir_FV = '~/remote/Hollywood2Data/fv/feats'; % Path where features will be saved
-    descriptor_path = '~/remote/Hollywood2Data/descriptor/'; % change paths here 
+    vocabDir = '/mnt/remote/Hollywood2Data/Vocab'; % Path where dictionary/GMM will be saved.
+    featDir_LLC = '/mnt/remote/Hollywood2Data/llc/feats'; % Path where features will be saved
+    featDir_FV = '/mnt/remote/Hollywood2Data/fv/feats'; % Path where features will be saved
+    descriptor_path = '/mnt/remote/Hollywood2Data/descriptor/'; % change paths here 
     
-    if ~exist(fullfile(descriptor_path),'dir')
+    if /mntexist(fullfile(descriptor_path),'dir')
             mkdir(fullfile(descriptor_path));
     end
 
-    video_dir = '~/remote/Hollywood2/';
-    video_data_dir = '~/remote/Hollywood2Data/';
+    video_dir = '/mnt/remote/Hollywood2/';
+    video_data_dir = '/mnt/remote/Hollywood2Data/';
     load('./train_test_split.mat');
     
     for i = 1 : length(fnames)
